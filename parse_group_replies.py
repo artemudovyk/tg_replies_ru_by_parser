3d411992e985ca0c0605d364ef5a488947727846
Artem Udovyk <udovyk.a@konstankino.com>
2022-03-06 04:46:11 +0200

    Set to send data to the server

--- parse_group_replies.py
+++ parse_group_replies.py
@@ -5,23 +5,27 @@ from telethon.errors.rpcerrorlist import MsgIdInvalidError
 from datetime import datetime, timedelta
 from google_sheets import write_to_google, read_all_phone_numbers
 import settings
+import requests
+import json
 
 # Use your own values from my.telegram.org
 api_id = settings.TELEGRAM_API_ID
 api_hash = settings.TELEGRAM_API_HASH
 
+base_server_url = 'http://127.0.0.1:8000'
+
 tg_groups = [
-    # 'toncoin_rus',
-    # 'DeCenter',
-    # 'forklog',
-    # 'givemetonru',
-    # 'incrypted',
-    # 'kriptokondrashov',
-    # 'prometheus',
-    # 'MinterNetwork',
-    # 'ikniga',
-    # 'alexanderpalienko',
-    # 'championat',
+    'toncoin_rus',
+    'DeCenter',
+    'forklog',
+    'givemetonru',
+    'incrypted',
+    'kriptokondrashov',
+    'prometheus',
+    'MinterNetwork',
+    'ikniga',
+    'alexanderpalienko',
+    'championat',
     'offsider',
     'myachPRO',
     'tribuna_by',
@@ -33,8 +37,31 @@ tg_groups = [
 
 client = TelegramClient('udovyk', api_id, api_hash)
 
+def save_to_db(phone, username):
+    url = f"{base_server_url}/post_contact/"
+
+    payload = {
+        "phone": phone,
+        "username": username
+    }
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+    
+    return response
+
+
+def get_all_phone_numbers():
+    url = f"{base_server_url}/get_phone_numbers/"
+
+    response = requests.request("GET", url)
+    phone_numbers = response.json()['phone_numbers']
+
+    return phone_numbers
+
+
 async def main():      
-    phone_numbers = set(read_all_phone_numbers())
+    phone_numbers = set(get_all_phone_numbers())
     print(phone_numbers)
     
     for group in tg_groups:
@@ -49,14 +76,17 @@ async def main():
                 async for reply in client.iter_messages(group, reply_to=message.id, limit=2000):
                     user = await reply.get_sender()
                     try:
-                        if user.phone and user.phone not in phone_numbers:
+                        if user.phone and user.phone not in phone_numbers and (user.phone.startswith('7') or user.phone.startswith('375')):
                             print(user.phone)
                             phone_numbers.add(user.phone)
-                            write_to_google(user.phone, user.username)
+                            save_to_db(user.phone, user.username)
                     except AttributeError:
                         continue
             except MsgIdInvalidError: 
                 continue
 
-with client:
-    client.loop.run_until_complete(main())
+
+if __name__ == "__main__":
+    # get_all_phone_numbers()
+    with client:
+        client.loop.run_until_complete(main())
