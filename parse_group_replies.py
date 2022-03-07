fbf7f946c8bfb2d1161c2844698333985508d0a0
Artem Udovyk <udovyk.a@konstankino.com>
2022-03-07 15:58:10 +0200

    Added save_to_db_batch method, added sleep time between requests

--- parse_group_replies.py
+++ parse_group_replies.py
@@ -6,13 +6,14 @@ from datetime import datetime, timedelta
 from google_sheets import write_to_google, read_all_phone_numbers
 import settings
 import requests
-import json
+import time
 
 # Use your own values from my.telegram.org
 api_id = settings.TELEGRAM_API_ID
 api_hash = settings.TELEGRAM_API_HASH
 
-base_server_url = 'http://127.0.0.1:8000'
+base_server_url = 'https://spam-okupantiv-tgbot.udovyk.dev'
+# base_server_url = 'http://127.0.0.1:8000'
 
 tg_groups = [
     'toncoin_rus',
@@ -46,7 +47,38 @@ def save_to_db(phone, username):
     }
     headers = {"Content-Type": "application/json"}
 
-    response = requests.request("POST", url, json=payload, headers=headers)
+    while True:
+        try:
+            response = requests.request("POST", url, json=payload, headers=headers)
+            break
+        except requests.exceptions.ConnectTimeout:
+            time.sleep(3)
+            
+    print(response)
+    
+    return response
+
+
+def save_to_db_batch(users):
+    url = f"{base_server_url}/post_contact_batch/"
+
+    payload = {
+        "users": users
+    }
+    headers = {"Content-Type": "application/json"}
+    
+    # print(payload)
+
+    while True:
+        try:
+            response = requests.request("POST", url, json=payload, headers=headers)
+            break
+        except requests.exceptions.ConnectTimeout:
+            time.sleep(3)
+            
+    print(response)
+    
+    print(response)
     
     return response
 
@@ -70,20 +102,28 @@ async def main():
         async for message in client.iter_messages(group, limit=3000):
             messages.append((group, message))
             
-        
+        stack_to_save_in_db = []
         for group, message in messages:
             try:
                 async for reply in client.iter_messages(group, reply_to=message.id, limit=2000):
                     user = await reply.get_sender()
                     try:
                         if user.phone and user.phone not in phone_numbers and (user.phone.startswith('7') or user.phone.startswith('375')):
-                            print(user.phone)
+                            print(user.phone, user.username)
                             phone_numbers.add(user.phone)
-                            save_to_db(user.phone, user.username)
+                            stack_to_save_in_db.append({
+                                "phone": user.phone, 
+                                "username": user.username
+                            })
+                            # print(f'{user.phone} skipped')
                     except AttributeError:
                         continue
             except MsgIdInvalidError: 
                 continue
+            
+            # Save all new contacts to db if it's not empty
+            if stack_to_save_in_db:
+                save_to_db_batch(stack_to_save_in_db)
 
 
 if __name__ == "__main__":
